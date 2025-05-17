f690fd4ae214fcd328881576b7125b9501b85250
rootTiket <l20078829@gmail.com>
2025-05-18 00:53:09 +0900

    feat: 핀 전체 조회 컨트롤러

--- src/main/java/com/sopkathon/teamweb/domain/pin/controller/PinController.java
+++ src/main/java/com/sopkathon/teamweb/domain/pin/controller/PinController.java
@@ -1,10 +1,13 @@
 package com.sopkathon.teamweb.domain.pin.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sopkathon.teamweb.domain.pin.dto.response.PinAllGetResponse;
 import com.sopkathon.teamweb.domain.pin.dto.response.PinGetResponse;
 import com.sopkathon.teamweb.domain.pin.service.PinService;
 import com.sopkathon.teamweb.global.common.dto.ResponseDto;
@@ -22,5 +25,12 @@ public class PinController {
 		PinGetResponse response = pinService.getPinDetail(pinId);
 
 		return ResponseDto.ok(response);
+	}
+
+	@GetMapping
+	public ResponseDto<List<PinAllGetResponse>> getPinAll() {
+		List<PinAllGetResponse> responses = pinService.getPinAll();
+
+		return ResponseDto.ok(responses);
 	}
 }
